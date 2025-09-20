b9b61f554d4fb4ddab3680b06715c92f1e148f2e
mel <67578661+aixizu@users.noreply.github.com>
2025-09-20 20:42:19 +0100

    custom hostname support

--- scripts/network_handler.gd
+++ scripts/network_handler.gd
@@ -1,17 +1,20 @@
 extends Node
 
-const IP_ADDRESS : String = "localhost"
+const IP_ADDRESS : String = "127.0.0.1"
 const PORT: int = 15155
 
 var peer : ENetMultiplayerPeer
+
+func _ready() -> void:
+	if OS.has_feature("server"):
+		start_server()
 
 func start_server() -> void:
 	peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer
 
-func start_client() -> void:
+func start_client(ip: String, port: int) -> void:
 	peer = ENetMultiplayerPeer.new()
-	peer.create_client(IP_ADDRESS, PORT)
+	peer.create_client(ip, port)
 	multiplayer.multiplayer_peer = peer
-	
--- scripts/network_ui.gd
+++ scripts/network_ui.gd
@@ -1,7 +1,33 @@
 extends Control
 
+@onready var ip: LineEdit = $VBoxContainer/IP
+@onready var status: Label = $VBoxContainer/Status
+
+func _ready() -> void:
+	if OS.has_feature("server"):
+		self.queue_free();
+
 func _on_client_pressed() -> void:
-	NetworkHandler.start_client()
+	status.text = "Status: Connecting..."
+	var text = ip.text
+	var parts = text.split(":")
+	var hostname = ""
+	var port = 0
+	
+	if parts.size() == 2:
+		hostname = parts[0]
+		port = int(parts[1])
+	elif parts.size() == 1:
+		status.text = "Status: hostname has no port !!"
+		return;
+	
+	if not hostname.is_empty():
+		NetworkHandler.start_client(hostname, port)
+		status.text = "Status: Connected ?"
+		return;
+	else:
+		status.text = "Status: no hostname !!"
+		return;
 
 func _on_server_pressed() -> void:
 	NetworkHandler.start_server()
